e3c773e2afcc94983230ee70b7d4d0d4f4f1c7f0
James Holland <James.Holland@milliman.com>
2019-07-01 11:17:29 -0400

    temporarily added path to test script

--- run_tests.bat
+++ run_tests.bat
@@ -17,6 +17,8 @@ call "%~dp0setup_env.bat"
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Seeding error level to zero
 set CI_ERRORLEVEL=0
 
+SET REFERENCE_DATA_PATHREF=K:\PHI\0273ZDM\3.000-0273ZDM02\xx-dev\test_reference_data\_testing_refdata\REFERENCE_DATA
+
 echo %~nx0 !DATE:~-4!-!DATE:~4,2!-!DATE:~7,2! !TIME!: Running SAS unit tests
 python "%PATH_HOTWARE_DRIVE%\Jenkins\batch_submit_sas_and_tail_log.py" scripts\Run_Unit_Tests.sas
 if !errorlevel! neq 0 set CI_ERRORLEVEL=!errorlevel!
